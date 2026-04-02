5d91e1390f22cae9738d41b0c91ffdbef7688d7d
phantekzy <mainilotfi@gmail.com>
2026-04-02 10:58:30 +0100

    prepared the path and bundled everything the the result

--- src/utils.rs
+++ src/utils.rs
@@ -7,11 +7,21 @@ pub struct ParsedUrl {
 pub fn parse_url(url: &str) -> Result<ParsedUrl, String> {
     let remainder = url
         .strip_prefix("http://")
-        .ok_or("Only Http is supported")?;
+        .ok_or("Only plain HTTP is supported in this zero-dependency version")?;
+
     let (host_port, path_query) = remainder.split_once('/').unwrap_or((remainder, ""));
+
     let (host, port) = if let Some((h, p)) = host_port.split_once(':') {
         (h.to_string(), p.to_string())
     } else {
         (host_port.to_string(), "80".to_string())
     };
+
+    let path = if path_query.is_empty() {
+        "/".to_string()
+    } else {
+        format!("/{}", path_query)
+    };
+
+    Ok(ParsedUrl { host, port, path })
 }
