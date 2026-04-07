1179b5415cb24fc374c98b1072bd562947eeebab
phantekzy <mainilotfi@gmail.com>
2026-04-07 11:32:47 +0100

    response section done

--- src/response.rs
+++ src/response.rs
@@ -1,2 +1,8 @@
-#[derive(Debug)]
-pub struct Response {}
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Response {
+    pub status: u16,
+    pub headers: HashMap<String, String>,
+    pub data: String,
+}
