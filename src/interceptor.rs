a50a8720cf14b386afdba72df18ea02f5b3cd4b6
phantekzy <mainilotfi@gmail.com>
2026-04-08 10:14:56 +0100

    initilized interceptors struct and contructor to to hold req and res interceptor vectors

--- src/interceptor.rs
+++ src/interceptor.rs
@@ -7,3 +7,12 @@ pub struct Interceptors {
     pub request: Vec<RequestInterceptor>,
     pub response: Vec<ResponseInterceptor>,
 }
+
+impl Interceptors {
+    pub fn new() -> Self {
+        Self {
+            request: Vec::new(),
+            response: Vec::new(),
+        }
+    }
+}
