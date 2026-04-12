925df195ad59c9d7f39e287a570e4368ab7346c6
phantekzy <mainilotfi@gmail.com>
2026-04-12 15:49:56 +0100

    The engine for URL decomposition. It detects schemes and assigns ports dynamically

--- src/utils.rs
+++ src/utils.rs
@@ -1,27 +1,39 @@
 pub struct ParsedUrl {
+    pub scheme: String,
     pub host: String,
     pub port: String,
     pub path: String,
 }
 
 pub fn parse_url(url: &str) -> Result<ParsedUrl, String> {
-    let remainder = url
-        .strip_prefix("http://")
-        .ok_or("Only Http is supported")?;
+    let (scheme, remainder) = if let Some(r) = url.strip_prefix("https://") {
+        ("https", r)
+    } else if let Some(r) = url.strip_prefix("http://") {
+        ("http", r)
+    } else {
+        return Err("Unsupported protocol. Use http:// or https://".into());
+    };
 
-    let (host_port, path_query) = remainder.split_once('/').unwrap_or((remainder, ""));
+    let (host_port, path_query) = match remainder.find('/') {
+        Some(pos) => (&remainder[..pos], &remainder[pos..]),
+        None => (remainder, "/"),
+    };
 
     let (host, port) = if let Some((h, p)) = host_port.split_once(':') {
         (h.to_string(), p.to_string())
     } else {
-        (host_port.to_string(), "80".to_string())
+        let default_port = match scheme {
+            "https" => "443",
+            "http" => "80",
+            _ => "80",
+        };
+        (host_port.to_string(), default_port.to_string())
     };
 
-    let path = if path_query.is_empty() {
-        "/".to_string()
-    } else {
-        format!("/{}", path_query)
-    };
-
-    Ok(ParsedUrl { host, port, path })
+    Ok(ParsedUrl {
+        scheme: scheme.to_string(),
+        host,
+        port,
+        path: path_query.to_string(),
+    })
 }
