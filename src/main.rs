f4930c900ebb16f0554df74fbba181fb1c2ac55f
phantekzy <mainilotfi@gmail.com>
2026-04-12 15:15:03 +0100

    support for custom request interceptors and headers

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,11 @@ use xsus::Xsus;
 
 fn main() {
     let mut client = Xsus::new("http://example.com");
+    client.interceptors.request.push(Box::new(|mut req| {
+        req.headers.insert(
+            "X-Custom-Auth".to_string(),
+            "Xsus-Security-Token".to_string(),
+        );
+        req
+    }));
 }
