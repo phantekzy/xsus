1be49c292e19d566ad9149421f26a5ce0c6d8c86
phantekzy <mainilotfi@gmail.com>
2026-04-12 08:40:06 +0100

    added a TCP connection with timeout and error mapping

--- src/transport.rs
+++ src/transport.rs
@@ -1,8 +1,14 @@
-use std::time::Duration;
+use std::{net::TcpStream, time::Duration};
 
 use crate::{error::XsusError, request::Request, utils::parse_url};
 
 pub fn execute_network_call(req: &Request, timeout: Duration) -> Result<String, XsusError> {
     let url_info = parse_url(&req.url).map_err(XsusError::InvalidUrl)?;
     let addr = format!("{}:{}", url_info.host, url_info.port);
+    let mut stream = TcpStream::connect_timeout(
+        &addr
+            .parse()
+            .map_err(|_| XsusError::Network("Could not resolve host".into()))?,
+        timeout,
+    )?;
 }
